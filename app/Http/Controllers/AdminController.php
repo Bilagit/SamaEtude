7527524524089ff441e5b359d7dbdf2376b6ebd8
unknown <ibrahimabila.diop@adie.sn>
2024-06-25 15:22:04 +0000

    fix add prof

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\AddProfesseurRequest;
+use App\Models\User;
 use App\Models\Professeur;
 
 class AdminController extends Controller
