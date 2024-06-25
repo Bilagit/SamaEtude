935220aee1246627dc2b247464dafa54d6152091
unknown <ibrahimabila.diop@adie.sn>
2024-06-25 17:01:01 +0000

    update

--- app/Http/Requests/UpdateProfilRequest.php
+++ app/Http/Requests/UpdateProfilRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AddProfesseurRequest extends FormRequest
+class UpdateProfilRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,11 +22,6 @@ class AddProfesseurRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
-            'first_name' => 'required',
-            'name' => 'required',
-            'password' => 'required',
-            'specialite' => 'required'
         ];
     }
 }
