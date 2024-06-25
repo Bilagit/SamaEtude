9cd17735f5f262bf44f5a2cdb835efd6ea2e8fd7
Abdou Aziz Sy <abdouazizsy@esp.sn>
2024-06-25 19:49:55 +0000

    categories page

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,4 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 Route::get('/profil', [AuthController::class, 'profil'])->name('auth.profil');
 Route::post('/addcategorie', [AdminController::class, 'addcategorie'])->name('admin.addcategorie');
 Route::get('/listecategorie', [AdminController::class, 'listecategorie'])->name('admin.listecategorie');
-Route::post('/deletecategorie/{id}', [AdminController::class, 'deletecategorie'])->name('admin.deletecategorie');
+Route::get('/deletecategorie/{id}', [AdminController::class, 'deletecategorie'])->name('admin.deletecategorie');
