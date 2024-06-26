122c54b6e05544d69cfcc2f4066816bd5b76b8b5
Abdou Aziz Sy <abdouazizsy@esp.sn>
2024-06-26 00:48:03 +0000

    profil admin update page 2

--- resources/views/Auth/profil.blade.php
+++ resources/views/Auth/profil.blade.php
@@ -105,13 +105,12 @@
                             </div>
                         </div>
                         <hr>
-
+                        @if ($user->role === 'etudiant' || $user->role === 'professeur')
                         <div class="row">
                             <div class="col-sm-3">
                                 <h6 class="mb-0">Téléphone</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
-                                @if ($user->role === 'etudiant' || $user->role === 'professeur')
                                     {{ $info->phone_number }}
                                 @else
                                     N/A
