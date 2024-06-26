537ba25d199432823d569c8f7ca6263b5cba6927
Ousseynou <ousseynouseck@esp.sn>
2024-06-26 21:21:32 +0000

    Ajouter Professeur

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js',   
                     'resources/css/sidebars.css', 'resources/js/sidebars.js',
-                    'resources/css/style.css',
+                    'resources/css/style.css', 'resources/css/main.css',
+                    'resources/css/util.css', 'resources/css/cours.js',
             ],
             refresh: true,
         }),
