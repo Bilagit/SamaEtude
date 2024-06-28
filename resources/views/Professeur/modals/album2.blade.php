35575ee9d08506f4b08edea320b9773f2a9366f7
Ousseynou <ousseynouseck@esp.sn>
2024-06-28 19:12:28 +0000

    Tous les cours 2

--- resources/views/Professeur/modals/album2.blade.php
+++ resources/views/Professeur/modals/album2.blade.php
@@ -1,42 +1,57 @@
-<div class="tab-content" id="nav-tabContent">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        @foreach ($cours as $cour)
-        <div class="col">
-            <div class="card shadow-sm">
-                <img src="{{ asset('img/java.webp') }}" class="rounded" width="100%" height="225" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        {{ $cour->nom }} - 
-                        @foreach ($categories as $categorie)
-                            @if ($categorie->id == $cour->idCategorie)
-                                {{ $categorie->nom }}
-                            @endif
+<style>
+.dot {
+  height: 10px;
+  width: 10px;
+  background-color: #00FF00;
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
+<div class="table-responsive small">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col">Cours</th>
+                <th scope="col">Catégorie</th>
+                <th scope="col">Instructeur</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($cours as $cour)
+            <tr>
+                <td><img src="{{ asset('img/java.webp') }}" class="rounded" width="20%" height="50" alt="">
+                <strong>{{ $cour->nom }}</strong> <br><small>Ajouté le : {{ $cour->created_at }}</small> </td>
+                <td><span class="dot"></span>
+                    @foreach ($categories as $categorie)
+                    @if ($categorie->id == $cour->idCategorie)
+                    {{ $categorie->nom }}
+                    @endif
+                    @endforeach
+                </td>
+                <td>
+                    <span
+                        class="badge d-flex align-items-center p-1 pe-2 text-success-emphasis bg-success-subtle border border-success-subtle rounded-pill">
+                        <img class="rounded-circle me-1" width="32" height="32" src="https://github.com/mdo.png" alt="">
+                        Pr. @foreach ($profs as $professeurs)
+                        @if ($professeurs->id == $cour->idProfesseur)
+                        @foreach ($users as $user)
+                        @if ($user->id == $professeurs->idUser)
+                        {{ $user->first_name }} {{ $user->name }}
+                        @endif
                         @endforeach
-                    </h5>
-                    <p class="card-text">{{ $cour->description }}</p>
-                    <p class="card-text"><strong>Professeur:</strong>
-                                @foreach ($profs as $professeurs)
-                                @if ($professeurs->id == $cour->idProfesseur)
-                                @foreach ($users as $user)
-                                @if ($user->id == $professeurs->idUser)
-                                {{ $user->name }} {{ $user->first_name }}
-                                @endif
-                                @endforeach
-                                @endif
-                                @endforeach
-                            </p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="{{ route('professeur.contenu', ['id' => $cour->id]) }}" class="btn btn-light">
-                                <img src="{{ asset('img/eye.svg') }}" width="16" height="16" alt="">
-                            </a>
-                        </div>
-                        <small class="text-body-secondary">Le
-                            {{ $cour->created_at }}</small>
+                        @endif
+                        @endforeach
+                    </span>
+                </td>
+                <td>
+                    <div class="btn-group">
+                        <a href="{{ route('professeur.contenu', ['id' => $cour->id]) }}" class="btn btn-light">
+                            <img src="{{ asset('img/eye.svg') }}" width="16" height="16" alt="">
+                        </a>
                     </div>
-                </div>
-            </div>
-        </div>
-        @endforeach
-    </div>
-</div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
