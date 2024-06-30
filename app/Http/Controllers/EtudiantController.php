b873045882af336b7c3e09c5e069571892154ff2
unknown <ibrahimabila.diop@adie.sn>
2024-06-30 14:07:04 +0000

    fix lister users dont prof pour etudiant

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cours;
 use App\Models\Professeur;
-use App\Models\Users;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EtudiantController extends Controller
@@ -12,7 +12,7 @@ class EtudiantController extends Controller
     public function index(){
         $cours = Cours::all();
         $profs = Professeur::all();
-        $users = Users::where('role', '=', 'professeur')->get();
+        $users = User::where('role', '=', 'professeur')->get();
         return view('etudiant.index', [
             'cours' => $cours,
             'profs' => $profs,
