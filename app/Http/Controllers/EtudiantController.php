f7261c38a5c95e29e5a6f728c998ad2ab98581b9
unknown <ibrahimabila.diop@adie.sn>
2024-06-30 13:58:28 +0000

    lister profs pour etudiant

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cours;
+use App\Models\Professeur;
 use Illuminate\Http\Request;
 
 class EtudiantController extends Controller
 {
     public function index(){
         $cours = Cours::all();
+        $profs = Professeur::all();
         return view('etudiant.index', [
-            'cours' => $cours
+            'cours' => $cours,
+            'profs' => $profs
         ]);
     }
 }
