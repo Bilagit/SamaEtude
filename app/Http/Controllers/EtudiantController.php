40e3be8e4e81e11b11a5e1693ff3115700aad274
unknown <ibrahimabila.diop@adie.sn>
2024-06-30 13:51:41 +0000

    lister cours pour etudiant

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cours;
 use Illuminate\Http\Request;
 
 class EtudiantController extends Controller
 {
     public function index(){
         return view('etudiant.index');
+    }
+    public function cours(){
+        $cours = Cours::all();
+        return view('etudiant.index', [
+            'cours' => $cours
+        ]);
     }
 }
