9287c92bdc2c7da8f601bae345fecc41d96cc520
unknown <ibrahimabila.diop@adie.sn>
2024-07-01 17:12:25 +0000

    about & contact controller, global count

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function contact(){
+        return view('contact');
+    }
+    public function about(){
+        return view('about');
+    }
 }
